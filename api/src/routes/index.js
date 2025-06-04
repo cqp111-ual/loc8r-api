4c34a8e11e18e323a62bd21d42630a84ece48637
Carlos Quesada Pérez <cqp111@inlumine.ual.es>
2025-06-04 23:41:34 +0200

    Test push webhook

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,9 +4,9 @@ const router = express.Router();
 router.get('/index', (req, res) => {
     return res.status(200).json({
       success: true,
-      message: 'Hey from index!',
+      message: 'API is working fine.',
       data: {
-        sample_data: 'abcd-jesus'
+        sample_data: 'Hello!!'
       }
     });
 });
